8a7d08b7eb6bb2a6994a46e9d0164858ca167d1a
clearloop <mercuryfletcher@gmail.com>
2021-05-08 21:33:55 +0800

    Note removed tests from pallet-contracts (#4)
    * fix(fixtures): the sandbox generator of the tests of fixtures
    
    * feat(fixtures): max_value_size
    
    * feat(tests): note removed tests
    
    * perf(tests): clean typo

---
+++ tests/README.md
@@ -0,0 +1,94 @@
+## Removed Tests
+
+Tests below are removed from `pallet-contracts`, because `ceres` doesn't care about these.
+
+
+### Account
+
+* ~~`calling_plain_account_fails`~~
+
+* ~~`account_removal_does_not_remove_storage`~~
+
+### Event
+
+* ~~`deposit_event_max_value_limit`~~
+
+
+### Gas
+
+* ~~`run_out_of_gas`~~
+
+* ~~`reinstrument_does_charge`~~
+
+
+### Storage
+
+* ~~`storage_size`~~
+
+* ~~`empty_kv_pairs`~~
+
+* ~~`storage_max_value_limit`~~
+
+
+### Fees
+
+* ~~`deduct_blocks`~~
+
+* ~~`default_rent_allowance_on_instantiate`~~
+
+* ~~`claim_surcharge_malus`~~
+
+* ~~`surcharge_reward_is_capped`~~
+
+* ~~`refcounter`~~
+
+
+### Removals
+
+* ~~`inherent_claim_surcharge_contract_removals`~~
+
+* ~~`signed_claim_surcharge_contract_removals`~~
+
+* ~~`lazy_removal_works`~~
+
+* ~~`lazy_removal_partial_remove_works`~~
+
+* ~~`lazy_removal_does_no_run_on_full_block`~~
+
+* ~~`lazy_removal_does_not_use_all_weight`~~
+
+* ~~`deletion_queue_full`~~
+
+* ~~`call_removed_contract`~~
+
+## Restoration
+
+* ~~`restorations_dirty_storage_and_different_storage`~~
+
+* ~~`restorations_dirty_storage`~~
+
+* ~~`restoration_different_storage`~~
+
+* ~~`restoration_code_evicted`~~
+
+* ~~`restoration_success`~~
+
+## Drain
+
+* ~~`cannot_self_destruct_through_draning`~~
+
+* ~~`cannot_self_destruct_while_live`~~
+
+* ~~`self_destruct_works`~~
+
+* ~~`destroy_contract_and_transfer_funds`~~
+
+* ~~`cannot_self_destruct_in_constructor`~~
+
+## Chain Extension
+
+* ~~`disabled_chain_extension_wont_deploy`~~
+
+* ~~`disabled_chain_extension_errors_on_call`~~
+
+* ~~`chain_extension_works`~~
--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -3,14 +3,20 @@ use ceres_ri::Instance as RI;
 use ceres_runtime::util;
 use ceres_sandbox::Sandbox;
 use ceres_seal::pallet_contracts;
+use ceres_std::Rc;
+use core::cell::{RefCell, RefMut};
 use parity_scale_codec::Encode;
 use parity_wasm::elements::Module;
 
 const ALICE: &str = "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d";
-// const BOB: &str = "0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48";
 
 /// Get a fixture and compile to instance
-fn fixture(name: &str) -> (Result<Instance<Sandbox>, ceres_executor::Error>, Sandbox) {
+fn fixture(
+    name: &str,
+) -> (
+    Result<Instance<Sandbox>, ceres_executor::Error>,
+    Rc<RefCell<Sandbox>>,
+) {
     let fixture_path = ["fixtures/", name, ".wat"].concat();
     let wasm = wat::parse_file(fixture_path).expect("Parse fixture failed");
 
@@ -20,39 +26,61 @@ fn fixture(name: &str) -> (Result<Instance<Sandbox>, ceres_executor::Error>, San
     // pack sandbox
     let limit = util::scan_imports(&el).expect("Scan imports failed");
     let mem = Memory::new(limit.0, limit.1).expect("New memory failed");
-    let mut sandbox = Sandbox::new(mem.clone(), Default::default());
-    sandbox
-        .tx
+    let sandbox = Rc::new(RefCell::new(Sandbox::new(mem.clone(), Default::default())));
+    let sandbox_cloned = sandbox.clone();
+    let mut bm = sandbox_cloned.borrow_mut();
+    bm.tx
         .set_caller(util::parse_code_hash(&ALICE).expect("Parse addr failed"));
 
     // Generate Instance
     let mut builder = Builder::new().add_host_parcels(pallet_contracts(RI));
     builder.add_memory("env", "memory", mem);
-    (Instance::new(&wasm, &builder, &mut sandbox), sandbox)
+    (Instance::new(&wasm, &builder, &mut bm), sandbox)
+}
+
+/// Shortcut of fxitures
+fn f(name: &str, f: fn(i: Instance<Sandbox>, s: RefMut<Sandbox>)) {
+    let (r, s) = fixture(name);
+
+    // set initialized balance
+    let mut bm = s.borrow_mut();
+    bm.tx.set_balance(1_000_000);
+
+    // deploy by default
+    let mut instance = r.expect("Instantiate wasm module failed");
+    instance
+        .invoke("deploy", &[], &mut bm)
+        .expect("Deploy failed");
+
+    // closures
+    f(instance, bm)
 }
 
 #[test]
 fn instantiate_and_call_deposit_event() {
     let (r, s) = fixture("return_from_start_fn");
     if let Err(_) = r {
-        assert_eq!(s.ret, Some(vec![1, 2, 3, 4]));
+        assert_eq!(s.borrow().ret, Some(vec![1, 2, 3, 4]));
     } else {
         panic!("Return data failed");
     }
 
-    assert_eq!(s.events, vec![(vec![], vec![1, 2, 3, 4])]);
+    assert_eq!(s.borrow().events, vec![(vec![], vec![1, 2, 3, 4])]);
 }
 
-// // TODO: invalid memory reference
-// //
+#[test]
+fn deposit_event_max_value_limit() {
+    f("event_size", |mut i, mut bm| {
+        bm.input = Some(bm.max_value_size().encode());
+        i.invoke("call", &[], &mut bm).expect("Invoke failed");
+
+        bm.input = Some((bm.max_value_size() + 1).encode());
+        assert!(i.invoke("call", &[], &mut bm).is_ok());
+    });
+}
+
 // #[test]
-// fn deposit_event_max_value_limit() {
-//     let (r, mut s) = fixture("event_size");
-//     let mut i = r.expect("Instantiate failed");
-//     s.tx.set_balance(30_000);
-//     i.invoke("deploy", &[], &mut s).expect("Deploy failed");
-//
-//     // Test max value size
-//     s.input = Some(s.max_value_size().encode());
-//     i.invoke("call", &[], &mut s).expect("Invoke failed");
-// }
+// fn call_return_code() {}
+
+// #[test]
+// fn instantiate_return_code() {}
