4904a70f6a625006499872616cdee91ce03776fb
clearloop <udtrokia@gmail.com>
2021-06-06 01:30:16 +0800

    test(metadata): fix the path of testing contract

--- tests/metadata.rs
+++ tests/metadata.rs
@@ -4,7 +4,7 @@ use ceres_runtime::Metadata;
 fn test_decoding_flipper() {
     assert!(
         serde_json::from_str::<Metadata>(&String::from_utf8_lossy(include_bytes!(
-            "../fixtures/flipper.contract"
+            "../contracts/flipper.contract"
         )))
         .is_ok()
     );
