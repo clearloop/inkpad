8da49661e14f165d41587c45d55e6cde1614179a
clearloop <mercuryfletcher@gmail.com>
2022-01-29 16:28:45 +0800

    chore(std): use Box from boxed

--- crates/std/src/no_std.rs
+++ crates/std/src/no_std.rs
@@ -2,9 +2,9 @@
 #![cfg(not(feature = "std"))]
 pub use alloc::{
     borrow::ToOwned,
+    boxed::Box,
     collections::BTreeMap,
     fmt, format,
-    prelude::v1::Box,
     rc::Rc,
     string::{String, ToString},
     vec,
